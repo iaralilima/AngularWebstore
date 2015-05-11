ced868d1502d47fa46d9c7f50224eadf7b6c9cbc
Jefferson Neves <jefferson.rpn@gmail.com>
2015-05-11 15:23:06 -0300

    Utilização de diretivas para inclusão de HTML

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -27,13 +27,8 @@
         this.carrinho = CarrinhoDAO.query();
 
         this.adicionarProduto = function(produto) {
-//            this.carrinho.produtos.push("teste");
-            CarrinhoDAO.create(produto);
-//          this.carrinho = CarrinhoDAO.query();
-//          console.log(this.carrinho.produtos);
-//          this.carrinho = CarrinhoDAO.query();
-//            this.carrinho.produtos.push(produto);
-            console.log(this.carrinho.produtos.push(produto));
+          CarrinhoDAO.create(produto);
+          this.carrinho.produtos.push(produto);
         };
         
         this.limparCarrinho = function() {
@@ -42,6 +37,11 @@
         };
     });
     
-    
+    app.directive('produtoTile', function() {
+      return {
+        restrict: 'A',
+        templateUrl: "inc/produto-tile.html"
+      };
+    });
     
 })();
